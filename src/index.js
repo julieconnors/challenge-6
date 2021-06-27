35c27a9a6f6951836ee917c6cc05864cc78146d5
Juliana Connors <65951445+julieconnors@users.noreply.github.com>
2021-06-27 06:19:29 -0400

    set state and event handler to move box

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import ScotchInfoBar from './ScotchInfoBar';
 import './styles.css';
 
 function App() {
+  const [offSet, setOffSet] = useState(300)
+
+  function moveBoxUp() {
+    setOffSet(offSet - 50);
+  }
+
   return (
     <div className="App">
       <h1>Move the Box!</h1>
 
       {/* handle the click event on this button */}
-      <button>👆 Move Up 👆</button>
+      <button onClick={moveBoxUp}>Move Up</button>
 
       {/* move this box using inline styles */}
-      <div className="box" style={{}} />
+      <div className="box" style={{transform: `translateY(${offSet}px)`}} />
 
       <ScotchInfoBar />
     </div>
